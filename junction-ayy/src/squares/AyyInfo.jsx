fa44541974dbb21e6640c0c620e7543cd590e1cc
Satria Sutisna <satrias@gmail.com>
2019-11-16 22:58:40 +0200

    ayyhello logo

--- junction-ayy/src/squares/AyyInfo.jsx
+++ junction-ayy/src/squares/AyyInfo.jsx
@@ -1,13 +1,21 @@
 import React, { Fragment } from "react";
 import code from "../assets/qr-some.svg";
+import logo from "../assets/ayyhello-logo.svg";
 
 export function AyyInfo() {
   return (
     <Fragment>
-      <div className="footer-left">kjhdf</div>
-      <div className="footer-middle">
-        <h2>AYYHELLO</h2>
+      <div className="footer-left">
+        <img
+          className="logo"
+          alt="ayyhello logo"
+          src={logo}
+          style={{ height: "72px" }}
+        />
       </div>
+      {/* <div className="footer-middle">
+        <h2>AYYHELLO</h2>
+      </div> */}
       <div className="footer-right flex">
         <p className="margin-r-small">
           Join the house discussion in @ayy-smt-6!
