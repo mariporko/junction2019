9859f3ac89ce95a186f66ddee7fa5ebebb17a304
smirq <the.mini.piglet@gmail.com>
2019-11-17 01:38:19 +0200

    Created Flask app with returning data analysis

--- ayy-lmao/analysis.py
+++ ayy-lmao/analysis.py
@@ -1,26 +1,60 @@
 import csv
+import json
+from datetime import date
 
 # Read data files skipping the metadata rows
 
 with open('SMT8-12_hourly.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
     counter = 0
+    hourly_data = {}
+    hourly_data['electricity'] = {}
+    hourly_data['heating'] = {}
+    hourly_data['water'] = {}
     for row in readCSV:
         if counter < 52:
             counter += 1
             continue
         else:
-            print(row[0], row[1], row[2],)
+            pass
+            #print(row[0], row[1], row[2],)
+
 
 with open('SMT8-12_weekly.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
+    weekly_data = {}
+    weekly_data['electricity'] = {}
+    weekly_data['heating'] = {}
+    weekly_data['water'] = {}
+    
     counter = 0
+
     for row in readCSV:
         if counter < 52:
             counter += 1
             continue
         else:
-            print(row[0], row[1], row[2],)
+            #if row[0] < "52":
+            #    key = int(row[0])
+            #else:
+            #    key = row[0]
+            if row[3]:
+                weekly_data['electricity'][row[0]] = int(row[3].replace(" ",""))
+            else:
+                weekly_data['electricity'][row[0]] = None
+
+            if row[9]:
+                weekly_data['heating'][row[0]] = float(row[9].replace(",",".").replace(" ",""))
+            else:
+                weekly_data['heating'][row[0]] = None
+            
+            if row[15]:
+                weekly_data['water'][row[0]] = float(row[15].replace(",",".").replace(" ",""))
+            else:
+                weekly_data['water'][row[0]] = None
+
+    #print(json.dumps(weekly_data, sort_keys=True, indent=4))
+            
 
 with open('SMT8-12_monthly.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
@@ -30,4 +64,9 @@ with open('SMT8-12_monthly.csv') as csvfile:
             counter += 1
             continue
         else:
-            print(row[0], row[1], row[2],)
+            pass
+            #print(row[0], row[1], row[2],)
+
+    
+
+
--- ayy-lmao/app.py
+++ ayy-lmao/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, jsonify
+from datetime import date
+
+import analysis 
+
+
 # from flask_sqlalchemy import SQLAlchemy
 # import pathlib
 
@@ -12,7 +17,7 @@ app = Flask(__name__)
 # class DataPoint(db.Model):
 #     idx = db.Column(db.Integer, primary_key=True)
 #     val = db.Column(db.Float)
-
+current_week = date.today().isocalendar()[1]
 
 @app.route('/')
 def index():
@@ -21,18 +26,31 @@ def index():
 
 @app.route('/el', methods=['GET'])
 def el():
-    return jsonify(name="electricity", data=[2, 4, 6, 8, 10, 12])
+    cur_week_data = analysis.weekly_data['electricity'][str(current_week)]
+    return jsonify(current_week=cur_week_data,
+        average = analysis.weekly_data['electricity']["Average"],
+        maximum = analysis.weekly_data['electricity']["Maximum"],
+        minimum = analysis.weekly_data['electricity']["Minimum"],
+        total = analysis.weekly_data['electricity']["Total"])
 
 
 @app.route('/heat', methods=['GET'])
 def heat():
-    return jsonify(name="heat", data=[2, 4, 6, 8, 10, 12])
-
+    cur_week_data = analysis.weekly_data['heating'][str(current_week)]
+    return jsonify(current_week=cur_week_data,
+        average = analysis.weekly_data['heating']["Average"],
+        maximum = analysis.weekly_data['heating']["Maximum"],
+        minimum = analysis.weekly_data['heating']["Minimum"],
+        total = analysis.weekly_data['heating']["Total"])
 
 @app.route('/water', methods=['GET'])
 def water():
-    return jsonify(name="water", data=[2, 4, 6, 8, 10, 12])
-
+    cur_week_data = analysis.weekly_data['water'][str(current_week)]
+    return jsonify(current_week=cur_week_data,
+        average = analysis.weekly_data['water']["Average"],
+        maximum = analysis.weekly_data['water']["Maximum"],
+        minimum = analysis.weekly_data['water']["Minimum"],
+        total = analysis.weekly_data['water']["Total"])
 
 @app.route('/dir', methods=['POST'])
 def dir_post():
