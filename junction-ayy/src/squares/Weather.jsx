03f16e28570a9c4b5b3c279766e27a174501a7f4
Satria Sutisna <satrias@gmail.com>
2019-11-16 16:29:45 +0200

    Remove card from time and weather

--- junction-ayy/src/squares/Weather.jsx
+++ junction-ayy/src/squares/Weather.jsx
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@material-ui/core/Card';
+import React, { useState, useEffect } from "react";
 
 export function Weather() {
+  const [data, setData] = useState({
+    temperature: 0,
+    weather_descriptions: [""],
+    feelslike: 0
+  });
 
-    const [data, setData] = useState({temperature: 0, weather_descriptions: [""], feelslike: 0});
+  useEffect(() => {
+    const request = require("request");
 
-    useEffect(() => {
-        const request = require('request');
+    let req = {
+      url:
+        "http://api.weatherstack.com/current?access_key=aa24982083f1c3d55aed3be87a73678f&query=Espoo",
+      method: "GET"
+    };
 
-        let req = {
-            url: 'http://api.weatherstack.com/current?access_key=aa24982083f1c3d55aed3be87a73678f&query=Espoo',
-            method: 'GET',
-        };
+    request(req, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        setData(JSON.parse(body).current);
+      }
+    });
+  }, []);
 
-        request(req, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                setData(JSON.parse(body).current);
-            }
-        });
-    }, []);
-
-    return (
-        <Card className="card weather">
-            <h2>{data.temperature} &#8451;</h2>
-            <p>(Feels more like {data.feelslike} &#8451;)</p>
-            <p>{data.weather_descriptions[0]}</p>
-        </Card>
-    );
+  return (
+    <div className="weather">
+      <h2>{data.temperature} &#8451;</h2>
+      <p>(Feels more like {data.feelslike} &#8451;)</p>
+      <p>{data.weather_descriptions[0]}</p>
+    </div>
+  );
 }
