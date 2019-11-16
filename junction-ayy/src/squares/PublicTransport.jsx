8cf72417561778c0f0b13d69cf8ee10db382e488
Satria Sutisna <satrias@gmail.com>
2019-11-16 17:28:28 +0200

    correct card header font

--- junction-ayy/src/squares/PublicTransport.jsx
+++ junction-ayy/src/squares/PublicTransport.jsx
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import bus from '../assets/bus.svg';
-import arrow from '../assets/arrow.svg';
-import { StyledCard } from '../StyledCard';
+import React, { useState, useEffect } from "react";
+import bus from "../assets/bus.svg";
+import arrow from "../assets/arrow.svg";
+import { StyledCard } from "../StyledCard";
 
 export function PublicTransport() {
+  const [data, setData] = useState([
+    {
+      headsign: "",
+      trip: { route: { shortName: "" } },
+      realtimeDeparture: 0,
+      serviceDay: 0
+    }
+  ]);
 
-    const [data, setData] = useState([{
-        headsign: "",
-        trip: { route: { shortName: "" } },
-        realtimeDeparture: 0,
-        serviceDay: 0,
-    }]);
+  useEffect(() => {
+    const request = require("request");
 
-    useEffect(() => {
-        const request = require('request');
-
-        let req = {
-            url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
-            method: 'POST',
-            headers: { "Content-Type": "application/graphql" },
-            body: `{
+    let req = {
+      url: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
+      method: "POST",
+      headers: { "Content-Type": "application/graphql" },
+      body: `{
                 stop(id: "HSL:2222210") {
                 name
                 stoptimesWithoutPatterns {
@@ -33,41 +34,45 @@ export function PublicTransport() {
                 }
                 }
             }`
-        };
+    };
 
-        request(req, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                setData(JSON.parse(body).data.stop.stoptimesWithoutPatterns);
-            }
-        });
-    }, []);
+    request(req, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        setData(JSON.parse(body).data.stop.stoptimesWithoutPatterns);
+      }
+    });
+  }, []);
 
-    function nextWillDepart(departs) {
-        if (data) {
-            let now = Math.floor((new Date() - new Date().setHours(0,0,0,0)) / 1000); 
-            let timeLeft = Math.floor((departs - now) / 100);
-            return timeLeft;
-        } else {
-            return ""
-        }
+  function nextWillDepart(departs) {
+    if (data) {
+      let now = Math.floor(
+        (new Date() - new Date().setHours(0, 0, 0, 0)) / 1000
+      );
+      let timeLeft = Math.floor((departs - now) / 100);
+      return timeLeft;
+    } else {
+      return "";
     }
+  }
 
-    return (
-        <StyledCard className="card travel">
-            <h3>Public transportation</h3>
-            <ul className="hsl-feed">
-                {data.map((departure, key) => {
-                    return (
-                        <li className="departure" key={key}>
-                            <img className="bus-icon" alt="BUS" src={bus} /> 
-                            <span className="bus-line">{departure.trip.route.shortName}</span>
-                            <img className="arrow-icon" alt="->" src={arrow} /> 
-                            <span className="bus-to">{departure.headsign}</span>
-                            <span className="departure-time">{nextWillDepart(departure.realtimeDeparture)}</span>
-                        </li>
-                    );
-                })}
-            </ul>
-        </StyledCard>
-    );
+  return (
+    <StyledCard className="card travel">
+      <h4>Public transportation</h4>
+      <ul className="hsl-feed">
+        {data.map((departure, key) => {
+          return (
+            <li className="departure" key={key}>
+              <img className="bus-icon" alt="BUS" src={bus} />
+              <span className="bus-line">{departure.trip.route.shortName}</span>
+              <img className="arrow-icon" alt="->" src={arrow} />
+              <span className="bus-to">{departure.headsign}</span>
+              <span className="departure-time">
+                {nextWillDepart(departure.realtimeDeparture)}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </StyledCard>
+  );
 }
