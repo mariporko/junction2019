9baaf5bf118d1faea576bd1c726bffbcbb632e1f
Porko Mari <mari.porko@aalto.fi>
2019-11-16 11:58:18 +0200

    HSL data about next bus leaving from Teekkarikylä working

--- junction-ayy/src/PublicTransport.jsx
+++ junction-ayy/src/PublicTransport.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import {
+    graphql,
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+  } from 'graphql';
 
 export function PublicTransport() {
 
+    const [next, setNext] = useState({
+        headsign: "",
+        trip: { route: { shortName: "" } },
+        realtimeDeparture: 0,
+        serviceDay: 0,
+    });
+
+    useEffect(() => {
+        const request = require('request');
+
+        let now = new Date().getSeconds();
+
+        let req = {
+            url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+            method: 'POST',
+            headers: { "Content-Type": "application/graphql" },
+            body: `{
+                stop(id: "HSL:2222210") {
+                name
+                stoptimesWithoutPatterns {
+                    realtimeDeparture
+                    trip {
+                        route {
+                            shortName
+                        }
+                    }
+                    headsign
+                }
+                }
+            }`
+        };
+
+        request(req, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                setNext(JSON.parse(body).data.stop.stoptimesWithoutPatterns[0]);
+            }
+        });
+    }, []);
+
+    function nextWillDepart() {
+        if (next) {
+            let departs = next.realtimeDeparture;
+            let now = Math.floor((new Date() - new Date().setHours(0,0,0,0)) / 1000);
+            let timeLeft = Math.floor((departs - now) / 100);
+            return timeLeft;
+        } else {
+            return ""
+        }
+    }
+
     return (
-        <h1>Public transport info</h1>
+        <Fragment>
+            <h1>Public transport info</h1>
+            <p>Bus {next.trip.route.shortName} to {next.headsign} will depart in {nextWillDepart()} minutes.</p>
+        </Fragment>
     );
 }
