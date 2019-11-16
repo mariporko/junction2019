b25a1940ea6141028db01a80e5bb7101f093b363
Satria Sutisna <satrias@gmail.com>
2019-11-16 22:32:35 +0200

    Card corrections

--- junction-ayy/src/squares/Messages.jsx
+++ junction-ayy/src/squares/Messages.jsx
@@ -7,34 +7,39 @@ import { StyledCard } from "../StyledCard";
 import { MESSAGES } from "../building_information";
 
 export function Messages() {
-
   return (
-    <StyledCard className="card social">
+    <StyledCard className="card-large social">
       <h2>Messages</h2>
       {MESSAGES.map((message, key) => {
         const classN = message.important ? "message important" : "message";
         const whichIcon = message.important ? faExclamation : faEnvelope;
 
-        return(
+        return (
           <div className={classN} key={key}>
-            <p className="text"><FontAwesomeIcon icon={whichIcon} /> {message.text}</p>
+            <p className="text">
+              <FontAwesomeIcon icon={whichIcon} /> {message.text}
+            </p>
             <p className="from"> {message.from}</p>
           </div>
-        )
+        );
       })}
     </StyledCard>
   );
 }
 
 export function MessagesSummary() {
-  const important = MESSAGES.filter(m => m.important).length;;
+  const important = MESSAGES.filter(m => m.important).length;
   const other = MESSAGES.length - important;
 
   return (
-    <StyledCard className="card social summary">
+    <StyledCard className="card-small social summary">
       <h4>Messages</h4>
-      <p><FontAwesomeIcon icon={faExclamation} /> {important} important messages</p>
-      <p><FontAwesomeIcon icon={faEnvelope} /> {other} other messags</p>
+      <p>
+        <FontAwesomeIcon icon={faExclamation} /> {important} important messages
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faEnvelope} /> {other} other messags
+      </p>
     </StyledCard>
   );
 }
